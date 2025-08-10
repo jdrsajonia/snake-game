91c41096b75cc8811ca8a1b7cf77a11090e03fcf
diego <jdrozosajonia@gmail.com>
2025-08-09 20:09:59 -0500

    code refactorized. Matrix attribute from boardObject and class node were removed

--- controller.py
+++ controller.py
@@ -1,21 +1,10 @@
 from objects import snakeObject, boardObject
-from os import system
 from time import sleep
 from pynput import keyboard
 
 last_key="d"
 
 DIMENSION=(11,30)
-
-def detect_apple(board: boardObject, snake: snakeObject, direction: tuple):
-    next_coordenates=snake._sum_vectors(snake.get_head_position(), snake.get_direction(direction))
-    should_grown=next_coordenates in board.apples_coordenates
-    if should_grown:
-        x,y=next_coordenates
-        board.apples_coordenates.remove(next_coordenates)
-        board.put_apple(2)
-    return should_grown
-
 
 def on_press(key):
     global last_key
@@ -31,28 +20,36 @@ listener=keyboard.Listener(on_press=on_press)
 listener.start()
 
 
-
+# ver si se pueden dejar los controles como un objeto y pasarselo a la serpiente por el constructor o por un metodo builder
 def controller():
-    # system("cls")
     print("\033[H\033[J", end="")
     snake=snakeObject(DIMENSION)
+    print(snake.keys)
+    # snake.set_controls("t","f","g","h")
+
+    print(snake.keys)
     board=boardObject(DIMENSION)
     board.put_apple(2)
 
-    while True:
+    while True: #probar si se pueden añadir mas serpientes y tableros
         
-        should_grown=detect_apple(board,snake,last_key)
-        snake.move_to(last_key, should_grown)
-        print(board.str_current_game(snake))
+        ##### tmp
+        # current_coordenate=snake.adjust_direction2(last_key)
+        # snake.set_current_direction(current_coordenate) # se actualiza la actual direccion de la serpiente
 
+        ##### tmp
+
+        should_grown=board.detect_apple(snake,last_key)
+        snake.move_to(last_key, should_grown) #esto define el ritmo del juego
+        print(board.str_current_game(snake))
+        
         if snake.colition:
             print("\033[H\033[J", end="")
-            board.serpent_character=board.colorize("x","b_red")
+            snake.serpent_character=snake.colorize("x","b_red")
             print(board.str_current_game(snake))
             break
 
         sleep(0.1)
-        # system("cls")
         print("\033[H\033[J", end="")  
 
 
--- objects.py
+++ objects.py
@@ -1,112 +1,44 @@
 from collections import deque
 from random import randint
 
-class node:
-    # probablemente quitar esta clase, ya que solo estamos trabajando con coordenadas
-    def __init__(self, position: tuple):
-        self.type=None
-        self.x, self.y = position
-    
-    def get_position(self):
-        return self.x, self.y
-    
-    def set_position(self, position: tuple):
-        self.x, self.y = position
-
-    def set_type(self, arg: str):
-        self.type=arg
-
 
 class snakeObject:
     def __init__(self, warp_dimensions: tuple):
 
         self.long=1
         self.body=deque()
-        self.head=node((0,0))
-        self.body.append(self.head.get_position())
+        self.head_position=(0,0)
+        self.body.append(self.head_position)
+
+        self.is_colorized=True
+        self.serpent_character=self.colorize("■","magenta")
 
         self.warp_x, self.warp_y = warp_dimensions
 
+        self.up, self.left, self.down, self.right = ("w", "a", "s", "d")
+
         self.keys={ 
-                    "w":(-1,0),     # arriba
-                    "a":(0,-1),     # izquierda
-                    "s":(1,0),      # abajo
-                    "d":(0,1)       # derecha
+                    self.up:(-1,0),     
+                    self.left:(0,-1),   
+                    self.down:(1,0),      
+                    self.right:(0,1)       
                     }
         
         self.current_direction=(0,1)
         self.colition=False
     
-    @staticmethod
-    def _sum_vectors(vector1:tuple, vector2:tuple):
-        new_vector = tuple((v1+v2) for v1, v2 in zip(vector1, vector2)) 
-        return new_vector
 
-    def get_direction(self, key:str):
-        new_direction=self.keys[key]  #!! controlador en la serpiente
-        if new_direction[0]==-self.current_direction[0] and new_direction[1]==-self.current_direction[1]:
-            return self.current_direction
-        self.current_direction=new_direction 
-        return self.current_direction
+    # def set_controls(self,up, left, down, right):
+    #     self.up, self.left, self.down, self.right = up, left, down, right
+    #     self.keys={ 
+    #                 self.up:(-1,0),     
+    #                 self.left:(0,-1),    
+    #                 self.down:(1,0),      
+    #                 self.right:(0,1)       
+    #                 }
 
-
-    def set_head_position(self, coordenates: tuple):
-        self.head.set_position(coordenates)
-    
-
-    def get_head_position(self):
-        return self.head.get_position()
-    
-    
-    def _normalize_position(self,coordenate: tuple):
-        current_x,current_y=coordenate
-        current_x=current_x%self.warp_x
-        current_y=current_y%self.warp_y
-        return current_x, current_y
-    
-    def increment(self):
-        self.long+=1
-        tail_position=self.body[0]
-        self.body.append(tail_position)
-        
-
-    def detect_colition(self, new_coordenate):
-        if new_coordenate in self.body:
-            # raise ValueError("LA SERPIENTE HA CHOCADO CON SU PROPIO CUERPO!!!")
-            self.colition=True
-
-    def move_to(self, direction_key:str, grow_up=False):
-        actual_position=self.get_head_position()
-        current_direction=self.get_direction(direction_key) 
-
-        if grow_up:
-            self.increment()
-
-        new_position=self._sum_vectors(actual_position,current_direction)
-
-        new_position=self._normalize_position(new_position)
-        self.detect_colition(new_position)
-       
-        self.body.pop()     #NOTA: tambien con esto se podría incrementar el tamaño, simplemente bypaseando la linea con grow_up con (if not grow_up)
-        self.set_head_position(new_position)
-        self.body.appendleft(new_position)
-        
-
-    
-    
-
-
-
-class boardObject:
-    
-    def __init__(self,dimension):
-        self.x, self.y = dimension
-        self.matrix=self._generate_matrix(self.y, self.x)
-        self.apples_coordenates=set()
-
-        self.is_colorized=True
-
-        self.colors = {
+    def colorize(self, char: str, color:str):
+        colors = {
             "black": 30,
             "red": 31,
             "green": 32,
@@ -125,36 +57,120 @@ class boardObject:
             "b_white": 97,
             "":37
         }
-
-        self.horizontal_character = self.colorize("=","")
-        self.vertical_character   = self.colorize("|","")
-        self.serpent_character    = self.colorize("■","magenta")
-        self.apple_character      = self.colorize("▫","green")
-        self.space_character      = " "
-        self.borders_character    = self.colorize("+","green")
-
-
-
-        self.horizontal_margin=self.borders_character+2*self.horizontal_character*(self.y)+self.borders_character
-
-    def colorize(self, char: str, color:str):
         if not self.is_colorized:
             return char
-        color=str(self.colors[color])
+        color=str(colors[color])
+        return "\033["+color+"m"+char+"\033[0m"
+    
+    @staticmethod
+    def _sum_vectors(vector1:tuple, vector2:tuple):
+        new_vector = tuple((v1+v2) for v1, v2 in zip(vector1, vector2)) 
+        return new_vector
+
+    #NOTA: intentar que la dirección actualizada sea un atributo de la serpiente para que board no requiera este metodo prestado, sino el atributo ya actualizado de la serpiente
+    def adjust_direction(self, key:str):
+        new_direction=self.keys[key]  #!! controlador en la serpiente
+        if new_direction[0]==-self.current_direction[0] and new_direction[1]==-self.current_direction[1]:
+            return self.current_direction
+        self.current_direction=new_direction 
+        return new_direction
+
+   
+    def get_head_position(self):
+        return self.head_position
+    
+
+    def set_head_position(self, coordenates: tuple):
+        self.head_position=coordenates
+    
+
+    def _normalize_position(self,coordenate: tuple):
+        current_x,current_y=coordenate
+        current_x=current_x%self.warp_x
+        current_y=current_y%self.warp_y
+        return current_x, current_y
+    
+
+    def detect_colition(self, new_coordenate):
+        if new_coordenate in self.body:
+            self.colition=True
+
+
+    def move_to(self, direction_key:str, grow_up=False):
+        actual_position=self.get_head_position()
+        current_direction=self.adjust_direction(direction_key) 
+
+        new_position=self._sum_vectors(actual_position,current_direction)
+
+        new_position=self._normalize_position(new_position)
+        self.detect_colition(new_position)
+
+        if not grow_up:
+            self.body.pop()     # crece = no elimina su cola al moverse
+        else:
+            self.long+=1 
+
+        self.set_head_position(new_position)
+        self.body.appendleft(new_position)
+        
+    
+class boardObject:
+    
+    def __init__(self,dimension):
+        # self.matrix=self._generate_matrix(self.y, self.x)
+        self.x, self.y = dimension
+        self.apples_coordenates=set()
+        self.is_colorized=True
+
+        # self.serpent_character    = self.colorize("■","magenta")
+    
+        self.char={
+            "h_line"            : self.colorize("=",""),
+            "v_line"            : self.colorize("|",""),
+            "serpent"           : self.colorize("■","magenta"),
+            "apple"             : self.colorize("▫","green"),
+            "space"             : " ",
+            "corner"            : self.colorize("+","green"),
+        }
+
+        self.horizontal_margin=self.char["corner"]+2*self.char["h_line"]*(self.y)+self.char["corner"]
+
+    def colorize(self, char: str, color:str):
+        colors = {
+            "black": 30,
+            "red": 31,
+            "green": 32,
+            "yellow": 33,
+            "blue": 34,
+            "magenta": 35,
+            "cyan": 36,
+            "white": 37,
+            "b_black": 90,
+            "b_red": 91,
+            "b_green": 92,
+            "b_yellow": 93,
+            "b_blue": 94,
+            "b_magenta": 95,
+            "b_cyan": 96,
+            "b_white": 97,
+            "":37
+        }
+        if not self.is_colorized:
+            return char
+        color=str(colors[color])
         return "\033["+color+"m"+char+"\033[0m"
 
-    @staticmethod
-    def _generate_matrix(row:int,column:int):
-        space=None
+    # @staticmethod
+    # def _generate_matrix(row:int,column:int):
+    #     space=None
 
-        rows=[space for i in range(row)]
-        matrix=[rows.copy() for j in range(column)]
-        return matrix
+    #     rows=[space for i in range(row)]
+    #     matrix=[rows.copy() for j in range(column)]
+    #     return matrix
 
     # los metodos relacionados con las manzanas, es posible que toque colocarlos en un controlador a parte para evitar el acoplamiento
     def put_apple(self, quantity, coordenate_restrictions: set=None):
         for i in range(quantity):
-
             if coordenate_restrictions==None:
                 random_x, random_y = randint(0,self.x-1), randint(0,self.y-1)
                 new_apple_coordenates=(random_x,random_y)
@@ -171,24 +187,34 @@ class boardObject:
             # self.matrix[random_x][random_y]="▫"
     
 
-    def str_current_game(self, snake: snakeObject=None):
-
+    def str_current_game(self, snake: snakeObject=None): # metodo que renderiza la escena (ver si se puede separar de boardObject)
         ascci_string=""
-
-
         ascci_string+=self.horizontal_margin+"\n"
 
         for i in range(self.x):
-            ascci_string+=self.vertical_character
+            ascci_string+=self.char["v_line"]
             for j in range(self.y):
                 coordenate=(i,j)
                 if coordenate in snake.body:
-                    ascci_string+=self.serpent_character+" "
+                    ascci_string+=snake.serpent_character+" "
                 elif coordenate in self.apples_coordenates:
-                    ascci_string+=self.apple_character+" "
+                    ascci_string+=self.char["apple"]+" "
                 else:
-                    ascci_string+=self.space_character+" "
-            ascci_string+=self.vertical_character+"\n"
+                    ascci_string+=self.char["space"]+" "
+            ascci_string+=self.char["v_line"]+"\n"
 
         ascci_string+=self.horizontal_margin+"\n"
         return ascci_string
+    
+
+
+    def detect_apple(self, snake: snakeObject, direction: str): 
+                                                                                #next coordenates se puede dejar como atributo de snake, y este atributo se puede rescatar aqui
+        next_coordenates=snake._sum_vectors(snake.get_head_position(), snake.adjust_direction(direction))
+        should_grown=next_coordenates in self.apples_coordenates
+        if should_grown:
+            # x,y=next_coordenates
+            self.apples_coordenates.remove(next_coordenates)
+            quantity_apples=randint(1,2)
+            self.put_apple(quantity_apples)
+        return should_grown
